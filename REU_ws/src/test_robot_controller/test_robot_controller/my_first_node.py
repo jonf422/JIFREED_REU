59f0c95beefe6743a55979c4fc6c60db7dcf1f69
Jon <jonf422@gmail.com>
2026-05-19 18:36:36 -0400

    Timer callback test node

--- REU_ws/src/test_robot_controller/test_robot_controller/my_first_node.py
+++ REU_ws/src/test_robot_controller/test_robot_controller/my_first_node.py
@@ -6,10 +6,13 @@ class testNode(Node):
     def __init__(self):
         super().__init__("test_node")
         self.get_logger().info("ROS2 Test Node")
+
+        self.counter_ = 0
         self.create_timer(1.0, self.timer_cb)
     
     def timer_cb(self):
-        self.get_logger().info("Timer")
+        self.get_logger().info("Timer " + str(self.counter_))
+        self.counter_ += 1
 
 def main(args=None):
     rclpy.init(args=args)
