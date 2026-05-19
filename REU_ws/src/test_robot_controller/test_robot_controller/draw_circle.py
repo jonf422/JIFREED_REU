48a67f0d8c48ae5c38288c2311f3c87b8a268ea1
Jon <jonf422@gmail.com>
2026-05-19 18:37:08 -0400

    Create draw_circle node

---
+++ REU_ws/src/test_robot_controller/test_robot_controller/draw_circle.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+import rclpy
+from rclpy.node import Node
+
+
+def main(args=None):
+    rclpy.init(args=args)
+
+
+    
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
