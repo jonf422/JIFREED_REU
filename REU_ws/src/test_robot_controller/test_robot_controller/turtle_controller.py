20ae0e6fa2fdd23e3d95426e075cd8df39ba8b76
Jon <jonf422@gmail.com>
2026-05-21 11:01:31 -0400

    added service call

--- REU_ws/src/test_robot_controller/test_robot_controller/turtle_controller.py
+++ REU_ws/src/test_robot_controller/test_robot_controller/turtle_controller.py
@@ -3,6 +3,8 @@ import rclpy
 from rclpy.node import Node
 from geometry_msgs.msg import Twist
 from turtlesim.msg import Pose
+from turtlesim.srv import SetPen
+from functools import partial
 
 class turtleControllerNode(Node):
     def __init__(self):
@@ -23,7 +25,33 @@ class turtleControllerNode(Node):
             cmd.linear.x=5.0
             cmd.angular.z = 0.0
         
+        #typically don't want to call service so frequently
+        self.call_set_pen_service(255*pose.x/12, 255*pose.y/12, 255*abs(pose.theta)/12, 5, 0)
+        
         self.cmd_vel_publisher_.publish(cmd)
+
+    def call_set_pen_service(self, r, g, b, width, off):
+        client = self.create_client(SetPen, "/turtle1/set_pen")
+
+        while not client.wait_for_service(1.0):
+            self.get_logger().warning("Waiting for service...")
+        
+        request = SetPen.Request()
+        request.r = int(r)
+        request.g = int(g)
+        request.b = int(b)
+        request.width = int(width)
+        request.off = int(off)
+
+        future = client.call_async(request)
+        future.add_done_callback(partial(self.set_pen_cb))
+
+    def set_pen_cb(self, future):
+        try:
+            response = future.result()
+        except Exception as e:
+            self.get_logger().error("Service call failed: %r" % (e,))
+
     
 
 def main(args=None):
