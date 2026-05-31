2cb09400da6e5cdd1c9875c0b27eff67511bcaf3
Jon <jonf422@gmail.com>
2026-05-31 15:41:32 -0400

    Object counting

--- REU_ws/src/open-cv_test/lect2/object_counting.py
+++ REU_ws/src/open-cv_test/lect2/object_counting.py
@@ -1,5 +1,6 @@
 import cv2
 from ultralytics import YOLO
+import numpy
 
 path = 'skiers.mp4'
 cap = cv2.VideoCapture(path)
@@ -13,8 +14,18 @@ while cap.isOpened():
         pass
 
     results = model.track(frame, classes=[0], persist=True, verbose=False)
-
     annotated_frame = results[0].plot()
 
-    for r in results:
+    if results[0].boxes and results[0].boxes.id is not None:
+        ids = numpy.asarray(results[0].boxes.id)
+        for oid in ids:
+            unique_ids.add(oid)
+        cv2.putText(annotated_frame, f'Count: {len(unique_ids)}', (10,30), cv2.FONT_ITALIC, 1, (0,255,0), 2)
+    cv2.imshow('Object Counting', annotated_frame)
+    
+    if cv2.waitKey(1) & 0xFF == 27:  # press ESC to exit
+        break
+
+cap.release()
+cv2.destroyAllWindows()
         
