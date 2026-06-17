4a84cd36273f67652cfb0a17dda067544b7adef6
Jon <jonf422@gmail.com>
2026-06-17 10:36:37 -0400

    bug fix

--- REU_ws/src/data_collection/data_collection/teleop_node.py
+++ REU_ws/src/data_collection/data_collection/teleop_node.py
@@ -22,8 +22,8 @@ import tty
 import atexit
 
 # Import your custom modules from this package
-from simple_teleop import sabertooth as st
-from simple_teleop.PID import PID
+import sabertooth as st
+from PID import PID
 
 # Terminal Arrow Key Escape Sequences
 UP = '\x1b[A'
