70fad00cf0659396a895c52af7bbe2f9ebd55910
Jon <jonf422@gmail.com>
2026-07-17 11:53:55 -0400

    created waypoint action client

--- REU_ws/src/weed_detection/weed_detection/control/mission_manager_node.py
+++ REU_ws/src/weed_detection/weed_detection/control/mission_manager_node.py
@@ -1,13 +1,12 @@
 import rclpy
 from rclpy.node import Node
 from rclpy.action import ActionClient
-from weed_interfaces.action import ToolCommand
+from weed_interfaces.action import WaypointCommand
 from enum import Enum, auto
 
 class State(Enum):
-    IDLE = auto()
-    HOMING = auto()
-    DRILLING = auto()
+    INITIALIZING = auto()
+    WAYPOINT = auto()
     DONE = auto()
     FAILED = auto()
 
@@ -15,40 +14,45 @@ class MissionManagerNode(Node):
     def __init__(self):
         super().__init__('mission_manager_node')
 
-        #set state
-        self.state = State.IDLE
+        self.declare_parameter('latitude', 0.0)
+        self.declare_parameter('longitude', 0.0)
 
-        self.action_pending = False
+        self.lat = self.get_parameter('latitude').value
+        self.lon = self.get_parameter('longitude').value
+
+        #set state
+        self.state = State.INITIALIZING
+
         self.action_done = False
         self.action_code = None
         
         self._timer = self.create_timer(1/10, self.tick) #10 hz
 
-        self.tool_client = ActionClient(self, ToolCommand, 'tool_command')
+        self.waypoint_client = ActionClient(self, WaypointCommand, 'waypoint_command')
 
     # -----------------------------------
     # State Machine
     # -----------------------------------
     def tick(self):
-        if self.state == State.IDLE:
-            if self.tool_client.server_is_ready():
-                self.transition(State.HOMING)
-                self.send_tool_cmd('home')
-        
-        elif self.state == State.HOMING:
-            if self.action_done:
-                if self.action_code == 0:
-                    self.transition(State.DRILLING)
-                    self.send_tool_cmd('drill')
+        if self.state == State.INITIALIZING:
+            if self.all_nodes_ready():
+                if self.waypoint_client.server_is_ready():
+                    if self.lat and self.lon != 0.0:
+                        self.send_waypoint_cmd([self.lat,self.lon])
+                    else:
+                        self.action_done = True
+                        self.action_code = 0
+                    self.transition(State.WAYPOINT)
                 else:
                     self.transition(State.FAILED)
-
-        elif self.state == State.DRILLING:
+        
+        elif self.state == State.WAYPOINT:
             if self.action_done:
                 if self.action_code == 0:
                     self.transition(State.DONE)
                 else:
                     self.transition(State.FAILED)
+    
 
         elif self.state == State.DONE:
             self.get_logger().info('Mission COMPLETE')
@@ -65,20 +69,24 @@ class MissionManagerNode(Node):
         self.get_logger().info(f'{self.state.name} -> {next_state.name}')
         self.state = next_state
 
+    # -----------------------------------
+    # Initialization Check
+    # -----------------------------------
+    def all_nodes_ready(self):
+        return True
 
     # -----------------------------------
-    # Drill Functions
+    # Navigation Functions
     # -----------------------------------
-    def send_tool_cmd(self, cmd):
-        self.action_pending = True
+    def send_waypoint_cmd(self, cmd):
         self.action_done = False
         self.action_code = None
 
-        goal = ToolCommand.Goal()
-        goal.command = cmd
-        self.get_logger().info(f"Dispatching '{cmd}'")
+        goal = WaypointCommand.Goal()
+        goal.coordinates = cmd
+        self.get_logger().info(f"Sending coordinates: {cmd}")
 
-        send_future = self.tool_client.send_goal_async(goal)
+        send_future = self.waypoint_client.send_goal_async(goal)
         send_future.add_done_callback(self.on_goal_response)
 
     
@@ -91,7 +99,6 @@ class MissionManagerNode(Node):
             self.get_logger().error("Goal rejected")
             self.action_done = True
             self.action_code = -1        # sentinel: tick treats as failure
-            self.action_pending = False
             return
         result_future = goal_handle.get_result_async()
         result_future.add_done_callback(self._on_result)
@@ -102,7 +109,6 @@ class MissionManagerNode(Node):
             f"Result: code {result.return_code} ({result.message})")
         self.action_code = result.return_code
         self.action_done = True
-        self.action_pending = False
 
 
 def main():
